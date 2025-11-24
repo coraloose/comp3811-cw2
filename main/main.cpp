909bfde6ae1206ad2526fa457a4454acdded7835
coraloose <148532336+coraloose@users.noreply.github.com>
2025-11-24 01:00:19 +0000

    Add files via upload

--- main/main.cpp
+++ main/main.cpp
@@ -115,12 +115,10 @@ int main() try
 	setup_gl_debug_output();
 #	endif // ~ !NDEBUG
 
-        // Global GL state
-        OGL_CHECKPOINT_ALWAYS();
+	// Global GL state
+	OGL_CHECKPOINT_ALWAYS();
 
-        glEnable( GL_DEPTH_TEST );
-        glDepthFunc( GL_LEQUAL );
-        glClearColor( 0.05f, 0.08f, 0.12f, 1.0f );
+	// TODO: global GL setup goes here
 
 	OGL_CHECKPOINT_ALWAYS();
 
@@ -172,12 +170,12 @@ int main() try
 		// Update state
 		//TODO: update state
 
-                // Draw scene
-                OGL_CHECKPOINT_DEBUG();
+		// Draw scene
+		OGL_CHECKPOINT_DEBUG();
 
-                glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		//TODO: draw frame
 
-                OGL_CHECKPOINT_DEBUG();
+		OGL_CHECKPOINT_DEBUG();
 
 		// Display results
 		glfwSwapBuffers( window );
